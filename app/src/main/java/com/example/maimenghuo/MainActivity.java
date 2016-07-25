0e6cc608356b43409bef56e628152f427702c198
1234 <734094320@qq.com>
2016-07-25 20:11:35 +0800

    组 吕 Monday

--- app/src/main/java/com/example/maimenghuo/MainActivity.java
+++ app/src/main/java/com/example/maimenghuo/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.maimenghuo;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.maimenghuo.adapter.MainPagerAdapter;
+import com.example.maimenghuo.framents.DiscoveyFragment;
+import com.example.maimenghuo.framents.HomeFragment;
+import com.example.maimenghuo.framents.PersonFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener
+         {
+
+
+    private ViewPager pager;
+    private MainPagerAdapter adapter;
+    private List<Fragment> data;
+    private List<ImageView> image;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //绿翠丽
+        initView();
+
+
+
     }
+
+
+
+    private void initView() {
+        pager = (ViewPager) findViewById(R.id.main_viewPager);
+        layout=(LinearLayout)findViewById(R.id.main_layout);
+        int count=layout.getChildCount();
+        image=new ArrayList<ImageView>();
+        data=new ArrayList<>();
+        HomeFragment homeFragment=new HomeFragment();
+        data.add(homeFragment);
+        DiscoveyFragment disFragment=new DiscoveyFragment();
+        data.add(disFragment);
+        PersonFragment personFragment=new PersonFragment();
+        data.add(personFragment);
+
+
+        adapter=new MainPagerAdapter(getSupportFragmentManager(),data);
+        pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(this);
+
+        for (int i = 0; i <count ; i++) {
+            final int j=i;
+            ImageView iv= (ImageView) layout.getChildAt(i);
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pager.setCurrentItem(j);
+                }
+            });
+            image.add(iv);
+        }
+
+
+    }
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        for (int i = 0; i < 3; i++) {
+            image.get(i).setSelected(false);
+            
+        }
+        image.get(position).setSelected(true);
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+}
+
+
+
 }
---
+++ app/src/main/java/com/example/maimenghuo/adapter/MainPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.maimenghuo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by lvcuili on 2016/7/25.
+ */
+public class MainPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragments;
+    public MainPagerAdapter(FragmentManager fm,List<Fragment> mFragments) {
+        super(fm);
+        this.mFragments=mFragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
